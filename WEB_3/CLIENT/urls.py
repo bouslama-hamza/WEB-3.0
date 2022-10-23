7fc653f2bbc73733b6201b4d54596b8ba0a79e28
bouslama-hamza <ham.bousa98@gmail.com>
2022-10-23 15:43:39 +0100

    Working on the app side

--- WEB_3/CLIENT/urls.py
+++ WEB_3/CLIENT/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.client_index, name='client-index'),
+    path('contact/' , views.client_contact , name='client-contact'),
+    path('about/' , views.client_about , name='client-about'),
+    path('service/' , views.client_service , name='client-service'),
+    path('dashboard/' , views.client_dashboard , name='client-dashboard'),
 ]
--- WEB_3/CLIENT/views.py
+++ WEB_3/CLIENT/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def client_index(request):
-    return render(request, 'client_index.html')
+    return render(request, 'HomePage/client_index.html')
+
+def client_contact(request):
+    return render(request , 'HomePage/client_contact.html')
+
+def client_about(request):
+    return render(request , 'HomePage/client_about.html')
+
+def client_service(request):
+    return render(request , 'HomePage/client_service.html')
+
+def client_dashboard(request):
+    return render(request , 'AppPage/client_dashboard.html')
