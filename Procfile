ad3cee080ae987d9667be36c707c26eeedcd32c5
bouslama-hamza <ham.bousa98@gmail.com>
2022-10-27 11:51:02 +0100

    Finish Application

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WEB_3.wsgi
--- WEB_3/settings.py
+++ WEB_3/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_on_heroku
+from re import S
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +28,7 @@ SECRET_KEY = 'django-insecure-2q0rxj3eer1@d-1+))xux*k#$_&9hrlzn^b08@_2g@-o@u44y9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['hosting-rsk.herokuapp.com']
 
 # Application definition
 
@@ -119,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
